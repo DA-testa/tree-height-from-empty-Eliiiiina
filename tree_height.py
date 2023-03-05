165ce1ecc86c2687b3d9636abb41e2cfa4de9c19
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-03-05 19:55:30 +0000

    ddd

--- tree_height.py
+++ tree_height.py
@@ -2,60 +2,69 @@
 
 import sys
 import threading
-import numpy
-import os
+import numpy as np
+
+# function to compute the height of the tree
+def compute_height(n, parents):
+    # create an empty tree as a list of lists
+    tree = [[] for i in range(n)]
     
-class Node:
-    def _init_(self, data):
-        self.data = data
-        self.children = []
-      
-def find_height(n, parent):
-    nodes = [Node(i) for i in range (n)]
-    root = None
-        
-    for i in range(n):
-        if parent[i] == -1:
-            root = nodes[i]
-        else:
-            parent_node = nodes[parent[i]]
-            parent_node.children.append(nodes[i])
-                
+    # fill the tree with children
+    for i, parent in enumerate(parents):
+        if parent != -1:
+            tree[parent].append(i)
+    
+    # initialize the root of the tree
+    root = np.where(parents == -1)[0][0]
+    
+    # initialize the queue with the root node
+    queue = [(root, 0)]
+    
+    # initialize the maximum height to 0
     max_height = 0
-    queue = [(root, 1)]
-   
+    
+    # traverse the tree using BFS
     while queue:
-        node, level = queue.pop(0)
-        if level > max_height:
-            max_height = level
-        for child in node.children:
-            queue.append((child, level + 1))
+        node, height = queue.pop(0)
+        # update the maximum height
+        max_height = max(max_height, height)
+        # add the children of the current node to the queue
+        for child in tree[node]:
+            queue.append((child, height + 1))
+    
     return max_height
 
-def main():
-    while True:
-        vai = input().strip().upper()
-        if vai == "I":
-            n = int(input())
-            parent = list(map(int, input().split()))
-            break
-        elif vai == "F":
-            fails = input()
-            if os.path.exists(fails):
-                with open(fails) as f:
-                    n = int(f.readline().strip())
-                    parent = list(map(int, f.readline().strip().split()))
-                break
-            else:
-                print()
-        else:
-            print()
-            
-    height = find_height(n, parent)
-    print(height)      
-    pass
 
-if _name_ == '_main_':
-    sys.setrecursionlimit(10**7)
-    threading.stack_size(2**27)
-    threading.Thread(target=main).start()
+# main function to read input and call the compute_height function
+def main():
+    # read the input type (keyboard or file)
+    text = input().strip()
+    
+    # read the input accordingly
+    if text == "I":
+        # read input from keyboard
+        n = int(input().strip())
+        parents_str = input().strip()
+        parents = np.array(list(map(int, parents_str.split())))
+        height = compute_height(n, parents)
+    elif text == "F":
+        # read input from file
+        file_name = "test/" + input().strip()
+        if "a" in file_name:
+            return
+        with open(file_name, 'r') as file:
+            n = int(file.readline().strip())
+            parents_str = file.readline().strip()
+            parents = np.array(list(map(int, parents_str.split())))
+            height = compute_height(n, parents)
+    
+    # output the height of the tree
+    print(height)
+
+
+# increase recursion limit and stack size for large input
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
+
+# create a new thread and start the main function
+threading.Thread(target=main).start()
