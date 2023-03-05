79cef181b8ac40ad61ec294b3795ae8197c3f26b
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-03-05 19:29:22 +0000

    da

--- tree_height.py
+++ tree_height.py
@@ -1,50 +1,5 @@
 # python3
 
-#import sys
-#import threading
-#import numpy as np
-
-
-#def compute_height (n, parents):
-    #tree=[[] for i in range (n)]
-    #for i, parent in enumerate(parents):
-      #  if parent!=-1:
-     #       tree[parent].append(i)
-
-    #root=np.where(parents==-1)[0][0]
-    #queue= [(root,0)]        
-    #max_height = 0
-    
-    #while queue:
-     # node,height=queue.pop(0)
-    #  max_height=max(max_height, height)
-   #   for child in tree[node]:
-  #   return max_height
-
-
-#def main():
- # text=input()
-#  if text[0]=="I":
-    #n=int(input())
-   # parents_str=input()
-  #  parents=np.array(list(map(int, parents_str.split())))
- #   height=compute_height(n, parents)
-#  elif text[0]=="F":
-    #file_name="test/"
-   # file_name=file_name+input()
-  #  if "a" in file_name:
-      #print("WRONG ANSWER")
- #     return
-#    with open(file_name,'r') as file:
-     # n=int(file.readline())
-    #  parents_str=file.readline().strip()
-   #   parents=np.array(list(map(int,parents_str.split())))
-  #    height=compute_height(n,parents)
- # print(height)           
-
-#sys.setrecursionlimit(10**7) 
-#threading.stack_size(2**27)   
-#threading.Thread(target=main).start()
 import sys
 import threading
 import numpy as np
@@ -97,16 +52,12 @@ def main():
         # read input from file
         file_name = "test/" + input().strip()
         if "a" in file_name:
-            #print("WRONG ANSWER")
             return
         with open(file_name, 'r') as file:
             n = int(file.readline().strip())
             parents_str = file.readline().strip()
             parents = np.array(list(map(int, parents_str.split())))
             height = compute_height(n, parents)
-    #else:
-       # print("Invalid input type")
-        #return
     
     # output the height of the tree
     print(height)
