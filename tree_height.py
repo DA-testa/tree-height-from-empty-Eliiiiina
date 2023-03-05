1bf4ac19aa6bf50e73afdb98c83e3b60ce8cf149
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-03-05 20:02:21 +0000

    d

--- tree_height.py
+++ tree_height.py
@@ -8,21 +8,21 @@ import numpy as np
 def compute_height(n, parents):
     # create an empty tree as a list of lists
     tree = [[] for i in range(n)]
-    
+
     # fill the tree with children
     for i, parent in enumerate(parents):
         if parent != -1:
             tree[parent].append(i)
-    
+
     # initialize the root of the tree
     root = np.where(parents == -1)[0][0]
-    
+
     # initialize the queue with the root node
     queue = [(root, 0)]
-    
+
     # initialize the maximum height to 0
     max_height = 0
-    
+
     # traverse the tree using BFS
     while queue:
         node, height = queue.pop(0)
@@ -31,7 +31,7 @@ def compute_height(n, parents):
         # add the children of the current node to the queue
         for child in tree[node]:
             queue.append((child, height + 1))
-    
+
     return max_height
 
 
@@ -39,7 +39,7 @@ def compute_height(n, parents):
 def main():
     # read the input type (keyboard or file)
     text = input().strip()
-    
+
     # read the input accordingly
     if text == "I":
         # read input from keyboard
@@ -57,7 +57,7 @@ def main():
             parents_str = file.readline().strip()
             parents = np.array(list(map(int, parents_str.split())))
             height = compute_height(n, parents)
-    
+
     # output the height of the tree
     print(height)
 
@@ -68,3 +68,4 @@ threading.stack_size(2**27)
 
 # create a new thread and start the main function
 threading.Thread(target=main).start()
+
