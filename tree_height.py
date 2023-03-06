6db0640157e55c47eb5798a26230c6922afba116
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-03-06 02:21:07 +0000

    test

--- tree_height.py
+++ tree_height.py
@@ -5,43 +5,50 @@ import threading
 import numpy as np
 
 
-def compute_height (n, parents):
-    tree=[[] for i in range (n)]
+def compute_height(n, parents):
+    tree = [[] for i in range(n)]
     for i, parent in enumerate(parents):
-        if parent!=-1:
+        if parent != -1:
             tree[parent].append(i)
 
-    root=np.where(parents==-1)[0][0]
-    queue= [(root,0)]        
+    root = np.where(parents == -1)[0][0]
+    queue = [(root, 0)]
     max_height = 0
-    
+
     while queue:
-        node,height=queue.pop(0)
-        max_height=max(max_height, height)
+        node, height = queue.pop(0)
+        max_height = max(max_height, height)
         for child in tree[node]:
-            queue.append((child,height+1))
+            queue.append((child, height + 1))
     return max_height
 
 
 def main():
-    text=input()
-    if text[0]=="I":
-        n=int(input())
-        parents_str=input()
-        parents=np.array(list(map(int, parents_str.split())))
-        height=compute_height(n, parents)
-    elif text[0]=="F":
-        file_name="test/"
-        file_name=file_name+input()
-    if "a" in file_name:
+    text = input()
+    if text[0] == "I":
+        n = int(input())
+        parents_str = input().strip()
+        parents = np.array(list(map(int, parents_str.split())))
+        height = compute_height(n, parents)
+    elif text[0] == "F":
+        file_name = "test/" + input().strip()
+        try:
+            with open(file_name, 'r') as file:
+                n = int(file.readline().strip())
+                parents_str = file.readline().strip()
+                parents = np.array(list(map(int, parents_str.split())))
+                height = compute_height(n, parents)
+        except FileNotFoundError:
+            print("Error: File not found")
+            return
+    else:
+        print("Error: Invalid input")
         return
-    with open(file_name,'r') as file:
-        n=int(file.readline())
-        parents_str=file.readline().strip()
-        parents=np.array(list(map(int,parents_str.split())))
-        height=compute_height(n,parents)
-    print(height)           
 
-sys.setrecursionlimit(10**7) 
-threading.stack_size(2**27)   
-threading.Thread(target=main).start()
+    print(height)
+
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(10 ** 7)
+    threading.stack_size(2 ** 27)
+    threading.Thread(target=main).start()
